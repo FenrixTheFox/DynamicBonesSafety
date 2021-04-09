b2826cecdc66bd0209c2e44eac55c6af3b7848dd
FenrixTheFox <82191882+FenrixTheFox@users.noreply.github.com>
2021-04-09 01:34:37 -0400

    Initial Mod

--- DBSMod.cs
+++ DBSMod.cs
@@ -1,8 +1,92 @@
-﻿using System;
+﻿using DynamicBonesSafety;
+using MelonLoader;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using UnhollowerRuntimeLib;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.UI;
+
+[assembly: MelonGame("VRChat", "VRChat")]
+[assembly: MelonInfo(typeof(DBSMod), "Dynamic Bones Safety", "2.0.0", "Fenrix")]
 
 namespace DynamicBonesSafety
 {
-    public class Class1
+    public class DBSMod : MelonMod
     {
+        public const int DynamicBonesEnumValue = 100;
+
+        public static Dictionary<string, bool> CanUseBones = new Dictionary<string, bool>();
+
+        private static VRCUiPageSafety _cachedPageSafety;
+        public static MethodInfo _cachedSelectedSafetyClass;
+        public static string UiPageSafetySelectedSocial => _cachedSelectedSafetyClass.Invoke(_cachedPageSafety, new object[] { }).ToString();
+
+        public override void OnApplicationStart()
+        {
+            // Generate Config
+            if (Directory.Exists("UserData")
+                && File.Exists("UserData/DynamicBonesSafety.json"))
+            {
+                CanUseBones = JsonConvert.DeserializeObject<Dictionary<string, bool>>(
+                    File.ReadAllText("UserData/DynamicBonesSafety.json"));
+            }
+            else
+            {
+                Directory.CreateDirectory("UserData");
+            }
+
+            _cachedSelectedSafetyClass = typeof(VRCUiPageSafety).GetProperties().Where(prop => prop.PropertyType.IsEnum).First().GetGetMethod();
+            ModPatches.DoPatch(Harmony);
+        }
+
+        public override void VRChat_OnUiManagerInit()
+        {
+            GameObject SafetyUiMenu = GameObject.Find("UserInterface/MenuContent/Screens/Settings_Safety");
+            _cachedPageSafety = SafetyUiMenu.GetComponent<VRCUiPageSafety>();
+
+            Transform SafetyMatrixTogglesTransform = GameObject.Find("UserInterface/MenuContent/Screens/Settings_Safety/_SafetyMatrix/_Toggles").transform;
+
+            // Rescale all children to make it not be  S Q U I S H
+            foreach (var t in SafetyMatrixTogglesTransform)
+            {
+                t.Cast<Transform>().localScale = new Vector3(0.9f, 0.9f, 0.9f);
+            }
+
+            GameObject DynamicBoneToggle = GameObject.Instantiate(GameObject.Find("UserInterface/MenuContent/Screens/Settings_Safety/_SafetyMatrix/_Toggles/Toggle_CustomAnimations"), SafetyMatrixTogglesTransform);
+            DynamicBoneToggle.name = "Toggle_DynamicBones";
+
+            DynamicBoneToggle.GetComponentInChildren<Text>().text = "Dynamic Bones";
+
+            UiSafetyFeatureToggle UiSafetyFeature = DynamicBoneToggle.GetComponent<UiSafetyFeatureToggle>();
+            UiSafetyFeature.field_Public_String_0 = "<i>Dynamic Bones</i> turns on or off dynamic bones on a user's avatar for the combined <i>Safety Mode</i> and <i>User Trust Rank</i>.";
+            UiSafetyFeature.GetType().GetProperties().Where(prop => prop.PropertyType.IsEnum).First().SetValue(UiSafetyFeature, DynamicBonesEnumValue);
+
+            Toggle BoneToggle = DynamicBoneToggle.GetComponent<Toggle>();
+
+            BoneToggle.onValueChanged.AddListener(DelegateSupport.ConvertDelegate<UnityAction<bool>>(new Action<bool>(delegate
+            {
+                CanUseBones[UiPageSafetySelectedSocial] = BoneToggle.isOn;
+                File.WriteAllText("UserData/DynamicBonesSafety.json", JsonConvert.SerializeObject(CanUseBones, Formatting.Indented));
+            })));
+
+            // Load our icon
+            // Taken directly from UIX
+            AssetBundle assetBundle;
+            using (var stream = Assembly.GetExecutingAssembly().GetManifestResourceStream("DynamicBonesSafety.dbsmod"))
+            using (var tempStream = new MemoryStream((int)stream.Length))
+            {
+                stream.CopyTo(tempStream);
+
+                assetBundle = AssetBundle.LoadFromMemory_Internal(tempStream.ToArray(), 0);
+                assetBundle.hideFlags |= HideFlags.DontUnloadUnusedAsset;
+            }
+
+            DynamicBoneToggle.GetComponentInChildren<RawImage>().texture = assetBundle.LoadAsset_Internal("boneIcon", Il2CppType.Of<Texture2D>()).Cast<Texture2D>();
+        }
     }
 }
---
+++ ModPatches.cs
@@ -0,0 +1,132 @@
+﻿using Harmony;
+using MelonLoader;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using UnhollowerBaseLib;
+using UnhollowerBaseLib.Attributes;
+using UnhollowerRuntimeLib.XrefScans;
+using UnityEngine;
+using VRC;
+using VRC.Core;
+using VRC.Management;
+using VRC.SDKBase;
+
+namespace DynamicBonesSafety
+{
+    class ModPatches
+    {
+        private static MethodInfo GetUserSocialRank = null;
+
+        public static void DoPatch(HarmonyInstance instance)
+        {
+            Type PermissionType = typeof(FeaturePermissionSet).GetNestedTypes()[0];
+
+            foreach (MethodInfo IsPermissionEnabled in typeof(FeaturePermissionSet).GetMethods(BindingFlags.Public | BindingFlags.Instance)
+                .Where(m => m.Name.StartsWith($"Method_Public_Boolean_{PermissionType.Name}")))
+                instance.Patch(IsPermissionEnabled, GetPatch(nameof(IsPermissionEnabled)));
+
+            foreach (MethodInfo AvatarFinishedLoading in typeof(VRCPlayer).GetMethods(BindingFlags.Public | BindingFlags.Instance)
+                .Where(m => m.Name.StartsWith("Method_Private_Void_GameObject_VRC_AvatarDescriptor_Boolean_") && !checkXref(m, "Avatar is Ready, Initializing")))
+                instance.Patch(AvatarFinishedLoading, null, GetPatch(nameof(AvatarFinishedLoadingPostfix)));
+
+            GetUserSocialRank = typeof(FeaturePermissionManager).GetMethods().Where(m => m.ReturnType.IsEquivalentTo(DBSMod._cachedSelectedSafetyClass.ReturnType) && !m.Name.Contains("_PDM_"))
+                .Where(m => m.GetCustomAttribute<CallerCountAttribute>().Count != 0)
+                .OrderBy(m => m.GetCustomAttribute<CallerCountAttribute>().Count)
+                .First();
+        }
+
+        private static bool IsPermissionEnabled(ref bool __result, int __0)
+        {
+            if (__0 != DBSMod.DynamicBonesEnumValue)
+                return true;
+
+            if (!DBSMod.CanUseBones.TryGetValue(DBSMod.UiPageSafetySelectedSocial, out __result))
+                __result = false;
+
+            return false;
+        }
+
+        private static void AvatarFinishedLoadingPostfix(VRCPlayer __instance, GameObject __0, VRC_AvatarDescriptor __1, bool __2)
+        {
+            if (__instance == null
+                || __0 == null
+                || __1 == null
+                || !__2)
+                return;
+
+            if (__instance == VRCPlayer.field_Internal_Static_VRCPlayer_0)
+                return;
+
+            APIUser apiUser = __instance.field_Private_Player_0.field_Private_APIUser_0;
+
+            if (apiUser == null)
+                return;
+
+            if (IsAvatarExplicityShown(apiUser.id))
+                return;
+
+            string selectedTrust = GetUserTrustRank(apiUser);
+
+            if (DBSMod.CanUseBones.ContainsKey(selectedTrust.ToString()))
+            {
+                if (!DBSMod.CanUseBones[selectedTrust.ToString()])
+                {
+                    Il2CppArrayBase<DynamicBoneCollider> dynamicBoneColliderComponents =
+                        __0.GetComponentsInChildren<DynamicBoneCollider>(true);
+                    Il2CppArrayBase<DynamicBone> dynamicBoneComponents =
+                        __0.GetComponentsInChildren<DynamicBone>(true);
+
+                    foreach (DynamicBoneCollider dynamicBoneCollider in dynamicBoneColliderComponents)
+                    {
+                        GameObject.DestroyImmediate(dynamicBoneCollider, true);
+                    }
+
+                    foreach (DynamicBone dynamicBone in dynamicBoneComponents)
+                    {
+                        GameObject.DestroyImmediate(dynamicBone, true);
+                    }
+                }
+            }
+        }
+
+        private static string GetUserTrustRank(APIUser apiUser)
+        {
+            return GetUserSocialRank.Invoke(FeaturePermissionManager.prop_FeaturePermissionManager_0, new object[] { apiUser }).ToString();
+        }
+
+        private static bool IsAvatarExplicityShown(string userId)
+        {
+            foreach (var playerModeration in ModerationManager.prop_ModerationManager_0.field_Private_List_1_ApiPlayerModeration_0)
+            {
+                if (playerModeration.moderationType == ApiPlayerModeration.ModerationType.ShowAvatar && playerModeration.targetUserId == userId)
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+        private static HarmonyMethod GetPatch(string name)
+        {
+            return new HarmonyMethod(typeof(ModPatches).GetMethod(name, BindingFlags.NonPublic | BindingFlags.Static));
+        }
+
+        private static bool checkXref(MethodBase m, string match)
+        {
+            try
+            {
+                return XrefScanner.XrefScan(m).Any(
+                    instance => instance.Type == XrefType.Global && instance.ReadAsObject() != null && instance.ReadAsObject().ToString()
+                                   .Equals(match, StringComparison.OrdinalIgnoreCase));
+            }
+            catch { } // ignored
+
+            return false;
+        }
+    }
+}
